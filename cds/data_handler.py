210490fa037b9155336f492a860cf1baaec95775
Caio Batista <caio.batista@gmail.com>
2022-06-19 16:17:08 -0700

    Fixed bug for updating rows on db

--- cds/data_handler.py
+++ cds/data_handler.py
@@ -13,8 +13,13 @@ class DataHandler(object):
         if force or not cached:
             data = City(geonameid)
             data.fetch_data()
-            new_city = self._table(geonameid=geonameid, data=data.to_json())
-            self._db.session.add(new_city)
+            if not cached:
+                # If it's a new city, create a new entry.
+                cached = self._table(geonameid=geonameid, data=data.to_json())
+            else:
+                # If it's an old city, update the existing entry.
+                cached.data = data.to_json()
+            self._db.session.add(cached)
             self._db.session.commit()
         else:
             data = City.from_json(cached.data)
